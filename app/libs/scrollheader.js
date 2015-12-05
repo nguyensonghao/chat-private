debe646bfae0ec83284577f201eb86c054000d08
nguyensonghao <nguyensonghao974@gmail.com>
2015-12-05 14:33:27 +0700

    Them fixed top cho menu

---
+++ app/libs/scrollheader.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	$(window).scroll(function () {
+		var X = $(this).scrollTop();
+		if (X > 104) {
+			$('.app-bar').css('display', 'none');
+			$('.navbar-fixed-top').addClass('navbar-fixed');
+			$('#main-contain').addClass('main-contain-fixed');
+		} else {
+			$('.app-bar').css('display', 'block');
+			$('.navbar-fixed-top').removeClass('navbar-fixed');
+			$('#main-contain').removeClass('main-contain-fixed');
+		}
+	})
+})
